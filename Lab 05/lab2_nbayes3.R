380cac2ae6950d8bd0fa3cf3e4e7aa4036bce07e
GurSimran Kaur Sujlana <123726696+GursimranSujlana@users.noreply.github.com>
2024-03-10 17:51:48 -0400

    Done Lab 05

---
+++ Lab 05/lab2_nbayes3.R
@@ -0,0 +1,9 @@
+require(mlbench)
+data(HouseVotes84)
+install.packages("klaR")
+library(klaR)
+model <- NaiveBayes(Class ~ ., data = HouseVotes84)
+predict(model, HouseVotes84[1:10,-1])
+
+pred <- predict(model, HouseVotes84[,-1])
+table(pred$class, HouseVotes84$Class)
